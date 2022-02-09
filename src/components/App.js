10a4f249d326155b7385f919d67fe41b169ff4b2
James Gale <james@Jamess-MacBook-Pro-2.local>
2022-02-09 16:34:22 -0500

    added more materialui

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Second from "./Second";
 import Start from "./Start";
-import SignIn from "./SignIn";
 const App = () => {
   const [message, setMessage] = useState([]);
 
@@ -12,9 +11,7 @@ const App = () => {
         <Route exact path="/">
           <Start message={message} setMessage={setMessage} />
         </Route>
-        <Route path="/second">
-          <SignIn />
-        </Route>
+        <Route path="/second"></Route>
       </Switch>
     </Router>
   );
