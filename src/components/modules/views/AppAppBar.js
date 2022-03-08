b11abe3547e1e41d3e3452b77e74c31ef4ac98e7
James Gale <james@Jamess-MacBook-Pro-2.local>
2022-03-08 18:53:38 -0500

    changed buttons

--- src/components/modules/views/AppAppBar.js
+++ src/components/modules/views/AppAppBar.js
@@ -31,16 +31,6 @@ function AppAppBar({ searchInput, setSearchInput }) {
               flex: 0,
             }}
           />
-          <div>Connected - {countCon}</div>
-          <input
-            className="search"
-            type="text"
-            placeholder="Search By GVR ID, Address, or GP Customer Number."
-            value={searchInput}
-            onChange={handleTextChange}
-          />
-          <div>Not Connected - {countDis} </div>
-
           <div className="drawer">
             <Button
               variant="contained"
@@ -66,6 +56,41 @@ function AppAppBar({ searchInput, setSearchInput }) {
               Clear
             </Button>
           </div>
+          <div>Connected - {countCon}</div>
+          <input
+            className="search"
+            type="text"
+            placeholder="Search By GVR ID, Address, or GP Customer Number."
+            value={searchInput}
+            onChange={handleTextChange}
+          />
+          <div>Not Connected - {countDis} </div>
+
+          {/* <div className="drawer">
+            <Button
+              variant="contained"
+              onClick={async () => {
+                await getInfo();
+                history.push("/second");
+                sessionStorage.setItem("site", JSON.stringify("12345"));
+
+                window.location.reload();
+              }}
+            >
+              Open Notes
+            </Button>
+          </div>
+          <div className="drawertwo">
+            <Button
+              variant="contained"
+              onClick={async () => {
+                history.push("/");
+                // window.location.reload();
+              }}
+            >
+              Clear
+            </Button>
+          </div> */}
         </Toolbar>
       </AppBar>
       <Toolbar />
