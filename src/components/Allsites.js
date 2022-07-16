a4127caea569c3e07a502453c50390ce02429f67
RhysonEden <jgaleiphone@gmail.com>
2022-07-16 14:00:38 -0400

    reload take 2

--- src/components/Allsites.js
+++ src/components/Allsites.js
@@ -74,11 +74,7 @@ const Allsites = ({ addSite, addEmail, createDisp }) => {
   const addData = (gvr_id, gp_cust, cus_name, site_address, contract) => {
     console.log(gvr_id, gp_cust, cus_name, site_address, contract);
     addSite(gvr_id, gp_cust, cus_name, site_address);
-    createDisp(gvr_id, gp_cust, cus_name, site_address, contract).then(
-      (resp) => {
-        console.log(resp);
-      }
-    );
+    createDisp(gvr_id, gp_cust, cus_name, site_address, contract);
     window.location.reload();
   };
 
