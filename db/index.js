4a583bb5a6a8215982ec3315c6c5328713591d26
RhysonEden <jgaleiphone@gmail.com>
2022-10-12 17:12:43 -0400

    fixing timestamps

--- db/index.js
+++ db/index.js
@@ -20,8 +20,8 @@ let date_ob = new Date();
 let date2 = ("0" + date_ob.getDate()).slice(-2);
 let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
 let year = date_ob.getFullYear();
-let hours = date_ob.getHours();
-let minutes = date_ob.getMinutes();
+let hours = date_ob.getHours() - 4;
+let minutes = (date_ob.getMinutes() < 10 ? "0" : "") + date_ob.getMinutes();
 let seconds = date_ob.getSeconds();
 
 async function getRecordByDate(start, end, gp) {
